b0f2005c8cc7a353dd0f087ec691d819a76dcf50
Yuto Ueki <ueki.yuto.bz@gmail.com>
2025-04-16 05:42:42 +0900

    remove

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
-import { z } from "zod";
 import { v4 as uuidv4 } from "uuid";
 
 // Create an MCP server
@@ -15,31 +14,9 @@ const server = new McpServer({
   },
 });
 
-// Add an addition tool
-server.tool(
-  "add",
-  "与えられた数値の足し算をする",
-  { a: z.number(), b: z.number() },
-  async ({ a, b }) => ({
-    content: [{ type: "text", text: String(a + b) }],
-  })
-);
-
 server.tool("uuid_generator", "UUIDを生成する", {}, async () => ({
   content: [{ type: "text", text: uuidv4() }],
 }));
-
-// // Add a dynamic greeting resource
-// server.resource(
-//   "greeting",
-//   new ResourceTemplate("greeting://{name}", { list: undefined }),
-//   async (uri, { name }) => ({
-//     contents: [{
-//       uri: uri.href,
-//       text: `Hello, ${name}!`
-//     }]
-//   })
-// );
 
 // Start receiving messages on stdin and sending messages on stdout
 const transport = new StdioServerTransport();
